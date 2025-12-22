f2ea2554ae8617f83f17bee160e0348821805617
dimasyoga42 <dimasyoga9090@gmail.com>
2025-12-23 00:04:07 +0700

    complite

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ import express from "express";
 import dotenv from "dotenv";
 import bodyParser from "body-parser";
 import Main from "./src/routes/main.js";
+import cors from "cors";
 dotenv.config();
 const app = express()
+app.use(cors({
+  origin: "*",
+  methods: "GET, POST, PUT, PATCH, DELETE",
+  credentials: true
+}))
 app.use("/", Main)
 
 
