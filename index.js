9ddc4d22d7260db0288311699822e9d83c32abb3
dimasyoga42 <dimasyoga9090@gmail.com>
2025-12-22 18:42:52 +0700

    complite

--- index.js
+++ index.js
@@ -4,14 +4,9 @@ import bodyParser from "body-parser";
 import Main from "./src/routes/main.js";
 dotenv.config();
 const app = express()
-app.use("/v1/api", Main)
+app.use("/", Main)
 
 
-app.get("/", (req, res) => {
-  res.json({
-    message: "testing server"
-  })
-})
 
 app.listen(3000, () => {
   console.log("running")
